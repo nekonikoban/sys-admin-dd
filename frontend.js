728d19b0942ce5d68e55b67f4aef8424df7d9cb4
Dario D <57863042+nekonikoban@users.noreply.github.com>
2024-01-02 19:32:21 +0100

    Styling, Animations & Tooltips*

--- frontend.js
+++ frontend.js
@@ -8,38 +8,27 @@ window.addEventListener("DOMContentLoaded", (e) => {
          loader.remove()
 
     // Section Highlighting and Connecting Lines on Hover
-    var sections = document.querySelectorAll('section');
-        var connectingLine = document.querySelector('.connecting-line');
+    var sections = document.querySelectorAll('section')
 
         sections.forEach(function (section) {
             section.addEventListener('mouseenter', function () {
-                // Highlight the current section
-                section.classList.add('section-highlighted');
+                section.classList.add('section-highlighted')
 
                 // Dim other elements
                 sections.forEach(function (otherSection) {
                     if (otherSection !== section) {
-                        otherSection.style.transition = 'opacity 0.3s ease-in-out';
-                        otherSection.style.opacity = '0.9';
+                        otherSection.style.transition = 'opacity 0.3s ease-in-out'
+                        otherSection.style.opacity = '0.9'
                     }
-                });
-
-                // Show connecting line
-                connectingLine.style.transition = 'opacity 0.3s ease-in-out';
-                connectingLine.style.opacity = '1';
-            });
+                })
+            })
 
             section.addEventListener('mouseleave', function () {
                 // Reset styles on mouse leave
                 sections.forEach(function (otherSection) {
-                    otherSection.style.transition = 'opacity 0.3s ease-in-out';
-                    otherSection.style.opacity = '1';
-                });
-
-                // Hide connecting line
-                connectingLine.style.transition = 'opacity 0.3s ease-in-out';
-                connectingLine.style.opacity = '0';
-            });
-        });
-
+                    otherSection.style.transition = 'opacity 0.3s ease-in-out'
+                    otherSection.style.opacity = '1'
+                })
+            })
+        })
 })
